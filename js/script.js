bfbf0fe1749e83d070436a0bb04167768917dfa2
Chloe Jones <112436912+chlooeanne@users.noreply.github.com>
2024-04-12 00:52:52 -0400

    finished contact form, privacy policy, events, etc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+window.addEventListener("load", function() {
+  let text = document.lastModified;
+  document.getElementById("demo").innerHTML = text;
+});
+
+// collapsible menu:
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active2");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 // script to make navbar sticky on scroll:
 window.onscroll = function() {
   make_sticky()
@@ -11,11 +32,16 @@ const newParentElement = document.querySelector('header');
 function make_sticky() {
   if (window.scrollY >= sticky + 5) {
     navbar.classList.add("sticky");
-    newParentElement.appendChild(elementToMove);
+    if (screen.width >= 768) {
+      newParentElement.appendChild(elementToMove);
+    }
+    
   }
   else {
     navbar.classList.remove("sticky");
-    document.querySelector('.logo').appendChild(elementToMove);
+    if (screen.width >= 768) {
+      document.querySelector('.logo').appendChild(elementToMove);
+    }
   }
 }
 
@@ -53,10 +79,14 @@ function handleChange(checkbox) {
   if (checkbox.checked == true) {
     document.querySelector("#main").style.display = "none";
     document.querySelector("#logo").style.display = "none";
+    document.querySelector("footer").style.display = "none";
+    document.querySelector("#current").style.color = "#FFCB05";
   }
   else {
     document.querySelector("#main").style.display = "block";
     document.querySelector("#logo").style.display = "block";
+    document.querySelector("footer").style.display = "block";
+    document.querySelector("#current").style.color = "white";
   }
 }
 
@@ -75,3 +105,4 @@ input.addEventListener("keypress", function(event) {
     document.querySelector(".next").click();
   }
 });
+
